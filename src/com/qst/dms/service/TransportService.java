e42d1274cab725c199810389ed884eef750bd183
shudal <shudal@qq.com>
2019-11-07 15:19:51 +0800

    first comit

---
+++ src/com/qst/dms/service/TransportService.java
@@ -0,0 +1,87 @@
+﻿package com.qst.dms.service;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Scanner;
+
+import com.qst.dms.entity.DataBase;
+import com.qst.dms.entity.MatchedLogRec;
+import com.qst.dms.entity.MatchedTransport;
+import com.qst.dms.entity.Transport;
+
+public class TransportService {
+	// 物流数据采集
+	public Transport inputTransport() {
+		Transport trans = null;
+
+		// 建立一个从键盘接收数据的扫描器
+		Scanner scanner = new Scanner(System.in);
+		try{
+			// 提示用户输入ID标识
+			System.out.println("请输入ID标识：");
+			// 接收键盘输入的整数
+			int id = scanner.nextInt();
+			// 获取当前系统时间
+			Date nowDate = new Date();
+			// 提示用户输入地址
+			System.out.println("请输入地址：");
+			// 接收键盘输入的字符串信息
+			String address = scanner.next();
+			// 数据状态是“采集”
+			int type = DataBase.GATHER;
+
+			// 提示用户输入登录用户名
+			System.out.println("请输入货物经手人：");
+			// 接收键盘输入的字符串信息
+			String handler = scanner.next();
+			// 提示用户输入主机IP
+			System.out.println("请输入 收货人:");
+			// 接收键盘输入的字符串信息
+			String reciver = scanner.next();
+			// 提示用于输入物流状态
+			System.out.println("请输入物流状态：1发货中，2送货中，3已签收");
+			// 接收物流状态
+			int transportType = scanner.nextInt();
+			// 创建物流信息对象
+			trans = new Transport(id, nowDate, address, type, handler, reciver,
+					transportType);
+		} catch (Exception e) {
+			System.out.println("采集的日志信息不合法");
+		}
+		// 返回物流对象
+		return trans;
+	}
+
+	// 物流信息输出
+	public void showTransport(Transport... transports) {
+		for (Transport e : transports) {
+			if (e != null) {
+				System.out.println(e.toString());
+			}
+		}
+	}
+
+	// 匹配的物流信息输出，可变参数
+	public void showMatchTransport(MatchedTransport... matchTrans) {
+		for (MatchedTransport e : matchTrans) {
+			if (e != null) {
+				System.out.println(e.toString());
+			}
+		}
+	}
+	// 匹配的物流信息输出，参数是集合
+	public void showMatchTransport(ArrayList<MatchedTransport> matchTrans) {
+		for (MatchedTransport e : matchTrans) {
+			if (e != null) {
+				System.out.println(e.toString());
+			}
+		}
+	}
+	
+	}
+	
+}
